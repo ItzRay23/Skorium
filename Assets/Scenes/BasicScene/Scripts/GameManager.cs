68006264ae0d7753217d7ac10d3f2473c592289b
Ray Ahmed <rayanahmed0108@gmail.com>
2025-04-23 22:47:21 -0400

    Fixed Ammo Counter

---
+++ Assets/Scenes/BasicScene/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+    public GameObject ammoCounter;
+    // public GameObject scoreCounter;
+    // public GameObject upgradePanel;
+
+    public void Start()
+    {
+        Player.ammoCount = 10;
+        updateUI();
+    }
+
+    public void changeAmmoCount(int value) 
+    {
+        Player.ammoCount += value;
+    }
+
+    public void changeScore(int value) 
+    {
+        Player.score += value;
+    }
+
+    public void updateUI()
+    {
+        Debug.Log("Ammo: " + Player.ammoCount.ToString());
+        ammoCounter.GetComponent<TextMeshPro>().text = "Ammo: " + Player.ammoCount.ToString();
+        
+    }
+}
